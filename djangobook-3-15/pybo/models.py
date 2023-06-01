6b810112b7c3d7f66bc06aa5c1b596df790e6ae4
chanwoo7 <chanwoo9987@gmail.com>
2023-06-01 21:23:28 +0900

    기존 feat/2 branch의 9f3317 커밋의 변경사항 반영 (댓글 추천 기능 구현)

--- djangobook-3-15/pybo/models.py
+++ djangobook-3-15/pybo/models.py
@@ -33,3 +33,4 @@ class Comment(models.Model):
     question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE)
     answer = models.ForeignKey(Answer, null=True, blank=True, on_delete=models.CASCADE)
     modify_count = models.IntegerField(default=0) # 댓글 수정 횟수를 나타내는 필드 추가
+    voter = models.ManyToManyField(User, related_name='voter_comment')
